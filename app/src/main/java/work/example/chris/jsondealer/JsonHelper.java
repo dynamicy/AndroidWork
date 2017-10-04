cad14cfc79c3465fc9a43d91868e1774c4632d6a
dynamicy <fermat.little.theorem@gmail.com>
2017-10-04 14:48:11 +0800

    Add jsonHelper.

---
+++ app/src/main/java/work/example/chris/jsondealer/JsonHelper.java
@@ -0,0 +1,83 @@
+package work.example.chris.jsondealer;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by chris on 04/10/2017.
+ */
+public class JsonHelper {
+
+    private final static String TAG = JsonHelper.class.getSimpleName();
+
+    public static String getJsonFromAssets(AssetManager assetManager, String filePath) {
+        String json = "";
+        InputStream inputStream = null;
+        try {
+            inputStream = assetManager.open(filePath);
+
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+
+            json = new String(buffer, "UTF8");
+        } catch (IOException ex) {
+            Log.e(TAG, "getJsonFromAssets: ", ex);
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException ex1) {
+                Log.e(TAG, "getJsonFromAssets: ", ex1);
+            }
+        }
+        return json;
+    }
+
+    public static String getJsonFromRaw(Context context) {
+        String json = "";
+        InputStream inputStream = null;
+
+        inputStream = context.getResources().openRawResource(R.raw.bill);
+
+        try {
+            int size = inputStream.available();
+
+            byte[] buffer = new byte[size];
+
+            inputStream.read(buffer);
+
+            json = new String(buffer, "UTF8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return json;
+    }
+
+    public static String getJsonFromRaw(Context context, int resource) {
+        String json = "";
+        InputStream inputStream = null;
+
+        inputStream = context.getResources().openRawResource(resource);
+
+        try {
+            int size = inputStream.available();
+
+            byte[] buffer = new byte[size];
+
+            inputStream.read(buffer);
+
+            json = new String(buffer, "UTF8");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return json;
+    }
+}
--- app/src/main/java/work/example/chris/jsondealer/MainActivity.java
+++ app/src/main/java/work/example/chris/jsondealer/MainActivity.java
@@ -2,12 +2,19 @@ package work.example.chris.jsondealer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String result = JsonHelper.getJsonFromRaw(this);
+
+        Log.d(TAG, "onCreate: " + result);
     }
 }
