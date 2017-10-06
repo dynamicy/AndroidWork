83dfa8abe5c23aca9e8eb691856e02f7b9881b6e
chrish <chrish@yxunistar.com>
2017-10-06 15:08:53 +0800

    Add EditActivity.

--- app/src/main/java/work/example/chris/jsondealer/MainActivity.java
+++ app/src/main/java/work/example/chris/jsondealer/MainActivity.java
@@ -50,8 +50,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, EditActivity.class);
+                startActivity(intent);
             }
         });
 
