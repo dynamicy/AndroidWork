cc6a5cd2ed11a4eb35562c7b019a3421d841cd26
dynamicy <fermat.little.theorem@gmail.com>
2017-11-28 19:03:50 +0800

    Add data contract and data models.

---
+++ JsonHelper/app/src/main/java/work/example/chris/jsonhelper/model/RecordSets.java
@@ -0,0 +1,16 @@
+package work.example.chris.jsonhelper.model;
+
+import java.util.List;
+
+public class RecordSets {
+
+    private List<Record> record;
+
+    public List<Record> getRecord() {
+        return record;
+    }
+
+    public void setRecord(List<Record> record) {
+        this.record = record;
+    }
+}
