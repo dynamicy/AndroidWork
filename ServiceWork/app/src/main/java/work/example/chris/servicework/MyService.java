30e0ee3dc874f9d524f3680f5a1d4b5f046386e9
dynamicy <fermat.little.theorem@gmail.com>
2017-12-03 00:06:26 +0800

    Testing service for oreo.

--- ServiceWork/app/src/main/java/work/example/chris/servicework/MyService.java
+++ ServiceWork/app/src/main/java/work/example/chris/servicework/MyService.java
@@ -3,14 +3,54 @@ package work.example.chris.servicework;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+
+import java.util.Calendar;
 
 public class MyService extends Service {
+
+    private final static String TAG = MyService.class.getSimpleName();
+
     public MyService() {
+        Log.d(TAG, "MyService: init");
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
+        Log.d(TAG, "onBind: ");
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind: ");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+
+        try {
+            Thread.sleep(5000);
+
+            Intent myIntent = new Intent(this, MainActivity.class);
+            startActivity(myIntent);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void onCreate() {
+        super.onCreate();
+
+        Log.d(TAG, "onCreate: ");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
 }
