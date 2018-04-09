358d47ef396e4cdf16cb1cb957cc792ce012e4b4
dynamicy <fermat.little.theorem@gmail.com>
2018-04-09 13:36:35 +0800

    Add the basic sample about glide.

--- ImageDownload/app/src/main/java/com/example/chrisswhuang/imagedownload/MainActivity.java
+++ ImageDownload/app/src/main/java/com/example/chrisswhuang/imagedownload/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.chrisswhuang.imagedownload;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
+
+    private void init() {
+        ImageView targetImageView = findViewById(R.id.imageView);
+
+        String internetUrl = "http://i.imgur.com/DvpvklR.png";
+
+        Glide.with(this).load(internetUrl).into(targetImageView);
+    }
+
 }
