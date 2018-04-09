507a4184262240385229428e91024756fc88b25f
dynamicy <fermat.little.theorem@gmail.com>
2018-04-09 15:47:21 +0800

    Thumbnail.

---
+++ ImageDownload/app/src/main/java/com/example/chrisswhuang/imagedownload/Image.java
@@ -0,0 +1,17 @@
+package com.example.chrisswhuang.imagedownload;
+
+import java.io.Serializable;
+
+public class Image implements Serializable {
+
+    private String imgName;
+
+    public String getImgName() {
+        return imgName;
+    }
+
+    public void setImgName(String imgName) {
+        this.imgName = imgName;
+    }
+
+}
--- ImageDownload/app/src/main/java/com/example/chrisswhuang/imagedownload/MainActivity.java
+++ ImageDownload/app/src/main/java/com/example/chrisswhuang/imagedownload/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.chrisswhuang.imagedownload;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String HOST = "http://i.imgur.com";
+    private static final String IMAGE_URL_PATTERN = "%s/%s";
+
+    private String getUrl(Image data) {
+        return String.format(Locale.getDefault(), IMAGE_URL_PATTERN, HOST, data.getImgName());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +30,46 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         ImageView targetImageView = findViewById(R.id.imageView);
 
-        String internetUrl = "http://i.imgur.com/DvpvklR.png";
+        Image thumbImage = genImageLists().get(2);
+        Image resultImage = genImageLists().get(3);
 
-        Glide.with(this).load(internetUrl).into(targetImageView);
+        Glide.with(this)
+                .load(getUrl(thumbImage))
+                .thumbnail(Glide.with(this)
+                        .load(getUrl(resultImage)))
+                .into(targetImageView);
+    }
+
+    private Image genDemoFile() {
+        Image tempImage = new Image();
+        tempImage.setImgName("DvpvklR.png");
+        return tempImage;
+    }
+
+    private List<Image> genImageLists() {
+        List<Image> imageList = new ArrayList<>();
+
+        Image image1 = new Image();
+        image1.setImgName("rFLNqWI.jpg");
+        imageList.add(image1);
+
+        Image image2 = new Image();
+        image2.setImgName("C9pBVt7.jpg");
+        imageList.add(image2);
+
+        Image image3 = new Image();
+        image3.setImgName("rT5vXE1.jpg");
+        imageList.add(image3);
+
+        Image image4 = new Image();
+        image4.setImgName("aIy5R2k.jpg");
+        imageList.add(image4);
+
+        Image image5 = new Image();
+        image5.setImgName("MoJs9pT.jpg");
+        imageList.add(image5);
+
+        return imageList;
     }
 
 }
